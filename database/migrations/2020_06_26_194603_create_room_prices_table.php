41118f31e165753b0a60a932f8edc915ba702a3f
edderai <a.edderai@gmail.com>
2020-06-27 17:27:37 +0000

    migration

---
+++ database/migrations/2020_06_26_194603_create_room_prices_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRoomPricesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if(! Schema::hasTable('room_prices')) {
+        Schema::create('room_prices', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->datetime('time_from')->nullable();
+            $table->datetime('time_to')->nullable();
+            $table->integer('price')->nullable();
+
+            $table->integer('categorie_id')->unsigned()->nullable();
+            $table->foreign('categorie_id')->references('id')->on('categories')->onDelete('cascade');
+        });
+    }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('room_prices');
+    }
+}
