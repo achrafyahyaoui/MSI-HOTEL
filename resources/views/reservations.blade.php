8b6b91838d1ea1f02da51fb6da439fdce567c227
Achraf Yahyaoui <achrafyahyaoui9@gmail.com>
2020-07-05 19:13:44 +0100

    just title..

--- resources/views/reservations.blade.php
+++ resources/views/reservations.blade.php
@@ -2,7 +2,7 @@
 <html>
 	<head>
 		<meta charset="utf-8">
-		<title>FormWizard_v2</title>
+		<title>MSI Hotel</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="author" content="colorlib.com">
 		<!-- LINEARICONS -->
